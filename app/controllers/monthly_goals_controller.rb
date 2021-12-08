3eac9632add1306e97fbefcb8f8ae3213b784ab7
muttta <93360763+muttta@users.noreply.github.com>
2021-12-08 09:02:59 +0900

    新規作成アクションの設定

--- app/controllers/monthly_goals_controller.rb
+++ app/controllers/monthly_goals_controller.rb
@@ -1,5 +1,19 @@
-class MonthlyGoalController < ApplicationController
-  def index
+class MonthlyGoalsController < ApplicationController
+  def new
     @monthly_goals = MonthlyGoal.new
+  end
+  def create
+    @monthly_goals = MonthlyGoal.new(monthly_params)
+    if @monthly_goals.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def monthly_params
+    params.require(:monthly_goal).permit(:day, :sales_expectation, :labor_cost, :event)
   end
 end
