298c024942bc66f2547fc13a3fa511049f7ecc6b
muttta <93360763+muttta@users.noreply.github.com>
2021-12-08 18:16:29 +0900

    マイグレーションファイル変更

--- app/models/daily_result.rb
+++ app/models/daily_result.rb
@@ -1,4 +1,4 @@
 class DailyResult < ApplicationRecord
   #belongs_to  :monthly_goal
-  validates :today, :sales, :productivity,   presence: true
+  validates :today, :sales, :man_hr,   presence: true
 end
