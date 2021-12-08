9dcaea66b64b61dbe553e5e75f54fe141a71f6bf
muttta <93360763+muttta@users.noreply.github.com>
2021-12-08 09:03:39 +0900

    バリデーションの変更

--- app/models/monthly_goal.rb
+++ app/models/monthly_goal.rb
@@ -1,3 +1,3 @@
 class MonthlyGoal < ApplicationRecord
-  validates :day, :sales_expectation, :labor_cost, :event, presence: true
+  validates :day, :sales_expectation, :labor_cost, presence: true
 end
