10fb4096b4a18c86b659a7fdf1205895e9aec228
muttta <93360763+muttta@users.noreply.github.com>
2021-12-08 17:54:38 +0900

    ビュー画面作成

--- app/models/daily_result.rb
+++ app/models/daily_result.rb
@@ -1,4 +1,4 @@
 class DailyResult < ApplicationRecord
-  belongs_to  :monthly_goal
-  validates :today, :sales, :rate, :productivity,   presence: true
+  #belongs_to  :monthly_goal
+  validates :today, :sales, :productivity,   presence: true
 end
